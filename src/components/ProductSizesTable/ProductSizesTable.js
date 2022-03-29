963d80bced17c50a6f47a2217a148168bebcbc6f
sqrniqer <68608815+sqrniqer@users.noreply.github.com>
2022-03-30 01:53:59 +0200

    as always small changes

--- src/components/ProductSizesTable/ProductSizesTable.js
+++ src/components/ProductSizesTable/ProductSizesTable.js
@@ -1,19 +1,47 @@
 import './ProductSizesTable.css'
+
+// ogarnac jakos jak zrobic komponent ktory generowalby tabele 
+
+
+
+
+let available = {
+  width: "110px",
+  height: "30px",
+  background: "#36CF66",
+}
+let temp_unavailable = {
+  width: "110px",
+  height: "30px",
+  background: "#F2DB05",
+}
+let unavailable = {
+  width: "110px",
+  height: "30px",
+  background: "#E01C15",
+}
+
 function TableCell(props){
-    // co powinna zawierac komorka
-    // śruby średnice i długość
     return (
-        <div className={"TableCell-" + props.diameter + "x" + props.length} >
-            
+        <div 
+        className={"TableCell-" + props.diameter + "x" + props.length} 
+        style={props.status}>
         </div>
     )
 }
 
-
-function ProductSizesTable() {
+function ProductSizesTable(props) {
+    let headers = [props.headers]
+    let rows = [props.rows]
+    
     return (
       <div className="product-main-table-container">
-        <TableCell diameter="M8" length="140"/>
+        {props.headers}
+        {props.rows}
+
+        <TableCell diameter="M8" length="140" status={available}/>
+        <TableCell diameter="M8" length="140" status={temp_unavailable}/>
+        <TableCell diameter="M8" length="140" status={unavailable}/>
       </div>
   
     );
--- src/pages/Frontpage.js
+++ src/pages/Frontpage.js
@@ -5,7 +5,10 @@ function Frontpage() {
     return (
       <div className="App">
         <MainpageProductShowcase  photo="/images/DIN931.png" Title='z łbem sześciokątnym'/>
-        <ProductSizesTable/>
+        <ProductSizesTable 
+        headers= {['M4', 'M5', 'M6', 'M8', 'M10']}
+        rows = {['30', '35', '40', '45', '50', '55', '60', '65']}
+        />
       </div>
   
     );
